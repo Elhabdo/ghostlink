50b2ecbdf401de60a633eb01c40348f103d904e7
Abdelmounaim Elhaila <abdelmounaimelhaila@gmail.com>
2020-10-15 14:40:31 +0100

    'fix_new_api'

--- website/views.py
+++ website/views.py
@@ -3,11 +3,13 @@ from django.http import HttpRequest
 import requests
 import json as simplejson
 
-_MYHOST = "https://www.ghostlnk.ga/"
+_MYHOST = "http://127.0.0.1:8000/"
 
 
 # _MYHOST = "http://www.ghostlnk.ga/"
 # Create your views here.
+
+
 
 def home(req):
     return render(req, "index.html", {})
@@ -17,8 +19,8 @@ def posst(req):
     url = req.POST["long_url"]
     url_f = shortcut(url)
     data = {
-        "url": url_f,
-        "old_url": url
+        "url": url_f[0],
+        "msg" : url_f[1]
     }
     return render(req, "shorted.html", data)
 
@@ -26,18 +28,28 @@ def posst(req):
 def shortcut(url: str):
     if not url.startswith("https://") or not url.startswith("https://"):
         url = "https://" + url
-    data = {"url": url}
-    response = requests.post('https://rel.ink/api/links/', json=data)
+    response = requests.get('https://ulvis.net/API/write/get?url='+ url)
     response_json = simplejson.loads(response.text)
-    url_short = response_json['hashid']
-    url_f = _MYHOST + url_short
-    return url_f
-
+    if "data" in response_json:
+        data = response_json["data"]
+        url_s = data["url"].split("/")[3]
+        print(url_s)
+        print(response_json)
+        url_f = _MYHOST+url_s
+        msg = "Your shorted url is ready"
+    else:
+        url_f = 'url invalide'.upper()
+        msg = "Your shorted url is not ready"
+    res = (url_f,msg)
+    print(res)
+    return res
 
 def open(req):
     if req.method == "GET":
         s_url = req.build_absolute_uri()
-        l_url = longurl(s_url)
+        sub_url = s_url.split("/")[3]
+
+        l_url = longurl(sub_url)
         if l_url == None:
             return render(req, "404.html", {})
         else:
@@ -47,11 +59,11 @@ def open(req):
 
 
 def longurl(url):
-    hashid = url.split("/")[3]
-    reqq = requests.get("https://rel.ink/api/links/" + hashid)
+    reqq = requests.get("https://ulvis.net/API/read/get?id=" + url)
     reqq_json = simplejson.loads(reqq.text)
-    if 'detail' in reqq_json:
-        return None
-    else:
-        url_long = reqq_json['url']
-        return url_long
+    print(reqq_json)
+    if "data" in reqq_json:
+        data = reqq_json["data"]
+        url_l = data["full"]
+        print(url_l)
+        return url_l
